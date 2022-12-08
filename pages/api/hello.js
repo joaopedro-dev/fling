c49dc4531d5d4b1d12921558bc64b5cdadb27ee4
João Pedro Barros <jpedro@server01.foursys.local>
2022-12-08 11:56:45 -0300

    Aterando API

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,13 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+  res.status(200).json(
+    {
+      id:1,
+      questao:'Qual super herói da Marvel você mais se identifica?',
+      alternatinaRegular:'Deadpool',
+      alternatinaReservada:'Homem-Aranha',
+      alternatinaEgocentrica:'Tony Stark',
+      alternatinaExemplar:'Capitão América',
+   })
 }
